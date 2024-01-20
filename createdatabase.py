2b59e90c456c6e9dbce1a20ebde8647a8de0848c
sandro.comerci <sandro.comerci@accenture.com>
2024-01-20 15:52:30 -0300

    update createdatabase

--- createdatabase.py
+++ createdatabase.py
@@ -2,6 +2,7 @@ from application import app, db
 from application.models import IncomeExpenses
 
 # Establece un contexto de aplicación
-with app.app_context():
-    # Crea todas las tablas de la base de datos
-    db.create_all()
+if __name__ == '__main__':
+    with app.app_context():
+        # Crea todas las tablas de la base de datos
+        db.create_all()
